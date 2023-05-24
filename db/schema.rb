cb812927133a5506ff1803cc623bebcf0aa07e1a
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-05-25 00:02:26 +0300

    feat: Posts table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_24_205019) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_24_205931) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "topic_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_posts_on_topic_id"
+  end
+
   create_table "topics", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "topics"
 end
