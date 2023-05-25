1930b948afa74d3415f6662113fd7f34887add13
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-05-25 15:43:16 +0300

    feat: Add error handlers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,22 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
 
+    # Rescue from  errors with a custom function
+    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
     def index
         render json: { message: "Rails API for my personal Blog" }
     end
 
+
+    private
+
+    def not_found record
+        render json: { error: record }, status: :not_found
+    end
+
+    def record_invalid invalid
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
 end
