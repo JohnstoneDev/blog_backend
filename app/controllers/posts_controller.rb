afb310b46717a64cdde57104c3ddff7997da26ce
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-05-25 15:43:45 +0300

    feat: Define post params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,11 @@ class PostsController < ApplicationController
 	def index
 		render json: Post.all
 	end
+
+
+	private 
+
+	def post_params
+		params.require(:title, :content).permit(:title, :content)
+	end 
 end
