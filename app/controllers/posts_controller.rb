7705b3d88f54e7d61fc8aa33ca249de3b85dc225
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-05-26 15:00:44 +0300

    feat: Define show & update method, create find_post helper

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,24 @@ class PostsController < ApplicationController
 		render json: post, status: :created
 	end
 
+	def show
+		render json: find_post(params[:id]), status: :ok
+	end
+
+	def update
+		post = find_post(params[:id])
+		post.update!(post_params)
+		render json: post, status: :ok
+	end
+
 
 	private
 
 	def post_params
 		params.permit(:title, :content, :topic_id)
+	end
+
+	def find_post id
+		post = Post.find(id)
 	end
 end
