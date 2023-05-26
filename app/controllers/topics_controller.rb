fbea89bc8faf1e5adeb5c37003670b38c31c82c6
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-05-26 14:15:08 +0300

    feat: Create method for topics that calls validation, define topic_params method

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,4 +2,15 @@ class TopicsController < ApplicationController
 	def index
 		render json: Topic.all
 	end
+
+	def create
+		topic = Topic.create!(topic_params)
+		render json: topic, status: :ok
+	end
+
+	private
+
+	def topic_params
+		params.permit(:name)
+	end
 end
